66f75cef83c0034b97c9113a45f992d1d7273f89
mikem <mikem@tarix.net>
2012-01-25 17:32:43 +0900

    install.cmd now removes old addon files

--- install.cmd
+++ install.cmd
@@ -1,6 +1,8 @@
 @ECHO OFF
 SET ANKI_ADDON_DIR=%USERPROFILE%\Documents\Anki\addons
 ECHO Installing MCD Support Addon to %ANKI_ADDON_DIR%
+DEL /Q %ANKI_ADDON_DIR%\mcdsupport.py 2> NUL
+RMDIR /S /Q %ANKI_ADDON_DIR%\mcd 2> NUL
 MKDIR %ANKI_ADDON_DIR%\mcd 2> NUL
 COPY /V /Y mcdsupport.py %ANKI_ADDON_DIR%\ > NUL
 COPY /V /Y mcd %ANKI_ADDON_DIR%\mcd\ > NUL
